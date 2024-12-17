c23dcd21318851f5ab6a85ed20822ee8b73d42c2
Dev_Amo <kiprutoamos44@gmail.com>
2024-12-17 21:00:07 +0300

    removed 'kl' in orderSummary.js

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -75,7 +75,7 @@ export function renderOrderSummary() {
     let html = '';
 
     deliveryOptions.forEach((deliveryOption) => {
-      const today = dayjs();kl;
+      const today = dayjs();
       const deliveryDate = today.add(
         deliveryOption.deliveryDays,
         'days'
